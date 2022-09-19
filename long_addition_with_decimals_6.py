c5390146f0b45f3da86344b8d1d7f8053be7bba3
SophisticatedPrimate <admin@sophisticatedprimate.com>
2022-09-19 17:05:45 -0500

    Add files via upload

---
+++ long_addition_with_decimals_6.py
@@ -0,0 +1,111 @@
+"""
+Check out the official python docs to learn more about python programming!
+https://docs.python.org/3/
+
+Refer to long_addition_with_decimals_1.py for further information on adding with decimals in python:
+https://github.com/sophisticatedprimate/long_addition_with_decimals_1/blob/main/long_addition_with_decimals_1.py
+
+To run the program: 
+    1. Open a terminal on a computer with python 3 installed
+    2. Navigate to a directory containing this file (long_addition_with_decimals_6.py)
+    3. Run the program with the following command: python3 long_addition_with_decimals_6.py
+    4. A print out of the sums should appear after the cursor
+
+If error:
+    a. make sure python is installed (run: python3 --version)
+    b. make sure you have the file installed and in the current directory
+    c. if you are still having issues or see any general issues in the programming please report using the contact information at end of file.
+"""
+
+# Problem 1 
+sum = 7911.8113 + 507.9055  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(7911.8113,507.9055,sum)) 
+
+# Problem 2 
+sum = 21920.9 + 23.5682  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(21920.9,23.5682,sum)) 
+
+# Problem 3 
+sum = 7411.111 + 5910.436  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(7411.111,5910.436,sum)) 
+
+# Problem 4 
+sum = 869.8124 + 1410.6176  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(869.8124,1410.6176,sum)) 
+
+# Problem 5 
+sum = 789.0181 + 842.5163  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(789.0181,842.5163,sum)) 
+
+# Problem 6 
+sum = 892.084 + 606.143  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(892.084,606.143,sum)) 
+
+# Problem 7 
+sum = 457.7112 + 525.4045  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(457.7112,525.4045,sum)) 
+
+# Problem 8 
+sum = 0.00005806 + 19.7083  
+print("{0:<10} + {1:<10} = {2:<11.8f}".format(0.00005806,19.7083,sum)) 
+
+# Problem 9 
+sum = 706.9116 + 3910.1132  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(706.9116,3910.1132,sum)) 
+
+# Problem 10
+sum = 578.426 + 708.453  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(578.426,708.453,sum)) 
+
+# Problem 11
+sum = 834.0112 + 194.7123  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(834.0112,194.7123,sum)) 
+
+# Problem 12
+sum = 45779.3 + 0.00470299  
+print("{0:<10} + {1:<10} = {2:<11.8f}".format(45779.3,0.00470299,sum)) 
+
+# Problem 13
+sum = 0.0986332 + 0.00136666  
+print("{0:<10} + {1:<10} = {2:<11.8f}".format(0.0986332,0.00136666,sum)) 
+
+# Problem 14
+sum = 54.8588 + 4.77097  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(54.8588,4.77097,sum)) 
+
+# Problem 15
+sum = 607.7014 + 609.114  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(607.7014,609.114,sum)) 
+
+# Problem 16
+sum = 748.1144 + 264.845  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(748.1144,264.845,sum)) 
+
+# Problem 17
+sum = 706.1124 + 455.7202  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(706.1124,455.7202,sum)) 
+
+# Problem 18
+sum = 0.00867969 + 0.759501  
+print("{0:<10} + {1:<10} = {2:<11.8f}".format(0.00867969,0.759501,sum)) 
+
+# Problem 19
+sum = 0.00235591 + 0.0069957  
+print("{0:<10} + {1:<10} = {2:<11.8f}".format(0.00235591,0.0069957,sum)) 
+
+# Problem 20
+sum = 6811.525 + 497.962  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(6811.525,497.962,sum)) 
+
+# Problem 21
+sum = 854.1151 + 4411.6165  
+print("{0:<10} + {1:<10} = {2:<11.5f}".format(854.1151,4411.6165,sum)) 
+
+"""
+Contact information
+
+General Queries: admin@sophisticatedprimate.com
+Report errors, bugs, and problems at:
+    email:  bug@sophisticatedprimate.com
+    github: https://github.com/sophisticatedprimate/long_addition_with_decimals_6/issues
+"""
